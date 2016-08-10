cf75053fcfd606378a0b0fa899e087ae2018e357
Jules Voltaire <javoltaire@wpi.edu>
2016-08-10 17:10:31 -0400

    routes for loging in and out and signups done and basic view done as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-	resources :users
+  #The root/welcome rout
+  root 'sessions#new'
+
+  #login in and out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  #Signup
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+
+
+
+
+
+
+
+
+
+	# resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160809213101) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
