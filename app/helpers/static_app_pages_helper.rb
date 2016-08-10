1c7c5c007d3ba75b7296a81d80c97e1a28c3ccf4
Jules Voltaire <javoltaire@wpi.edu>
2016-08-10 17:39:37 -0400

    Generated a static controller that will control tasks, notes and Calendar views

---
+++ app/helpers/static_app_pages_helper.rb
@@ -0,0 +1,2 @@
+module StaticAppPagesHelper
+end
---
+++ test/controllers/static_app_pages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StaticAppPagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
