7acbaf4724e945cae83a69863ce5d5f5169819f5
Jules Voltaire <javoltaire@wpi.edu>
2016-08-12 11:22:07 -0400

    basic listing of notes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'notes/index'
-
   #Signup
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
@@ -15,6 +13,9 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
+  #Notes
+  get '/notes' => 'notes#index'
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809213101) do
+ActiveRecord::Schema.define(version: 20160812143718) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "is_user_note"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
