70c904f67e2ac734b38dc69799a0f1fc7a454aa5
Jules Voltaire <javoltaire@wpi.edu>
2016-08-13 17:24:11 -0400

    Text_field highlighting on error works for sign up page

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160812143718) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
